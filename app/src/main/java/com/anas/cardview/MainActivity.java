346ea13b8b43a89203ee6758b066f4c80942b496
anas2002 <anas4112002@gmail.com>
2023-05-02 22:58:08 +0530

    final commit

--- app/src/main/java/com/anas/cardview/MainActivity.java
+++ app/src/main/java/com/anas/cardview/MainActivity.java
@@ -1,14 +1,30 @@
 package com.anas.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView=findViewById(R.id.cardView);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Card View clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
